da9204f4a3928ba43a215cbabe3da87f73a83f26
emanuelaromano98 <emauelaromano1998@gmail.com>
2024-07-27 00:46:03 +0200

    refactored repository

---
+++ notebooks/neighborhoods.py
@@ -0,0 +1,26 @@
+
+
+
+madrid_districts_dict = {
+    "Arganzuela": ["Atocha", "Imperial", "La Chopera", "Las Acacias", "Las Delicias", "Legazpi", "Palos de Moguer"],
+    "Barajas": ["Aeropuerto", "Alameda de Osuna", "Casco Histórico de Barajas", "Corralejos", "Timón"],
+    "Carabanchel": ["Abrantes", "Buenavista", "Comillas", "Opañel", "Puerta Bonita", "San Isidro", "Vista Alegre"],
+    "Centro": ["Cortes", "Embajadores", "Justicia", "Universidad", "Palacio", "Sol"],
+    "Chamartín": ["Castilla", "Ciudad Jardín", "El Viso", "Hispanoamérica", "Nueva España", "Prosperidad"],
+    "Chamberí": ["Almagro", "Arapiles", "Gaztambide", "Ríos Rosas", "Trafalgar", "Vallehermoso"],
+    "Ciudad Lineal": ["Atalaya", "Colina", "Concepción", "Pinar de Chamartín", "Pueblo Nuevo", "Quintana", "San Juan Bautista", "San Pascual", "Ventas"],
+    "Fuencarral-El Pardo": ["Barrio del Pilar", "El Goloso", "El Pardo", "Fuentelarreina", "La Paz", "Mirasierra", "Peñagrande", "Valverde"],
+    "Hortaleza": ["Apóstol Santiago", "Canillas", "Palomas", "Pinar del Rey", "Piovera", "Valdefuentes"],
+    "Latina": ["Aluche", "Campamento", "Cuatro Vientos", "Las Águilas", "Lucero", "Los Cármenes", "Puerta del Ángel"],
+    "Moncloa-Aravaca": ["Aravaca", "Argüelles", "Casa de Campo", "Ciudad Universitaria", "El Plantío", "Valdemarín", "Valdezarza"],
+    "Moratalaz": ["Fontarrón", "Horcajo", "Marroquina", "Media Legua", "Pavones", "Vinateros"],
+    "Puente de Vallecas": ["Entrevías", "Numancia", "Palomeras Bajas", "Palomeras Sureste", "Portazgo", "San Diego"],
+    "Retiro": ["Adelfas", "Estrella", "Ibiza", "Jerónimos", "Niño Jesús", "Pacífico"],
+    "Salamanca": ["Castellana", "Fuente del Berro", "Goya", "Guindalera", "Lista", "Recoletos"],
+    "San Blas-Canillejas": ["Amposta", "Arcos", "Canillejas", "Hellín", "Rejas", "Rosas", "Salvador", "Simancas"],
+    "Tetuán": ["Almenara", "Bellas Vistas", "Berruguete", "Castillejos", "Cuatro Caminos", "Valdeacederas"],
+    "Usera": ["Almendrales", "Moscardó", "Orcasitas", "Orcasur", "Pradolongo", "San Fermín", "Zofio"],
+    "Vicálvaro": ["Ambroz", "Casco Histórico de Vicálvaro", 'El Cañaveral', 'Valdebernardo', 'Los Berrocales', 'Los Cerros'],
+    "Villa de Vallecas": ["Casco Histórico de Vallecas", "Santa Eugenia"],
+    "Villaverde": ["Butarque", "Los Ángeles", "Los Rosales", "San Cristóbal de los Ángeles", "Villaverde Alto"]
+}
